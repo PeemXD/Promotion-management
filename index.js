a2a62de8224a69579fad15081c4e71a3f6d0ae54
Peem Auppati <104556421+PeemXD@users.noreply.github.com>
2023-03-09 20:28:29 +0700

    add gracefull shutdown

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import Rotes from "./Routes/Promotion.routes.js";
 
+process.on("SIGINT", () => {
+  console.log("Received SIGINT. Shutting down gracefully.");
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log("Server closed.");
+    process.exit(0);
+  });
+  setTimeout(() => {
+    console.error(
+      "Could not close connections in time, forcefully shutting down"
+    );
+    process.exit(1);
+  }, 10000);
+});
+
 const app = express();
 app.use(
   cors({
